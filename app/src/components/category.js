24900704eb985186b49ab479db89a370856472e9
Jae Jang <jaejang5566@gmail.com>
2019-05-08 00:44:28 -0700

    Removed unused constructor and changed endpoint

--- app/src/components/category.js
+++ app/src/components/category.js
@@ -3,11 +3,6 @@ import _ from 'lodash';
 
 class Category extends Component {
 
-    constructor(props) {
-        super(props);
-
-    }
-
     _onclick() {
         const change = _.get(this.props,`onclick`);
         const category = this.props.details;
--- app/src/values/endpoints.js
+++ app/src/values/endpoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NODE_ENV && process.env.NODE_ENV === "development" ? "http://localhost:8080/api" : "http://api.inventory.ga/api";
+const BASE_URL = process.env.MODE && process.env.MODE === "dev" ? "http://localhost:8080/api" : "http://api.inventory.ga/api";
 
 export const endpoint = {
     allcategories : BASE_URL + "/category",
